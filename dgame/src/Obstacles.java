7c1e2305697be7ecb74988c9d4945e098dc19cba
Mursal Ashraf <ashrafmursal@gmail.com>
2021-08-24 20:01:17 +1000

    added Obstacle class

---
+++ dgame/src/Obstacles.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Obstacles extends GameObject{
+
+    private ArrayList<String> obstaclePaths = new ArrayList<>();
+
+    public Obstacles(){
+        super(Game.GAME_WIDTH, Game.GAME_HEIGHT);
+
+        obstaclePaths.add("assets/assets/cactus1.png");
+        obstaclePaths.add("assets/assets/cactus2.png");
+        obstaclePaths.add("assets/assets/cactus3.png");
+
+        sprite = loadSprite(obstaclePaths.get(new Random().nextInt(obstaclePaths.size())));
+        
+    }
+}
