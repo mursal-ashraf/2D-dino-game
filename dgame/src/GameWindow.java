28c6a6c1d1d320e4273e44531069e11b77927ad6
Mursal Ashraf <ashrafmursal@gmail.com>
2021-08-24 17:51:12 +1000

    created JFrame object in GameWindow

--- dgame/src/GameWindow.java
+++ dgame/src/GameWindow.java
@@ -1,6 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GameWindow {
 
     public GameWindow (int w, int h, String title, Game newGame) {
+        newGame.setPreferredSize(new Dimension(w, h));
 
+        JFrame frame = new JFrame(title);
+        frame.add(newGame);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        newGame.start();
     }
 }
