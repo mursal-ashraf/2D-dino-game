c3612dd60ea4ceddd287293d1771bb0abeeecfef
Mursal Ashraf <ashrafmursal@gmail.com>
2021-08-24 19:22:49 +1000

    added GameObject class

---
+++ dgame/src/GameObject.java
@@ -0,0 +1,42 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
+
+public abstract class GameObject {
+
+    protected float x, y;
+    protected float dx, dy;
+
+    protected BufferedImage sprite;
+
+    public GameObject (float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void tick(){
+        x += dx;
+        y += dy;
+    }
+
+
+    public void render(Graphics g, ImageObserver observer){
+        // subtract sprite height because height starts at top-left
+        g.drawImage(sprite, (int) x, (int) y - sprite.getHeight(), observer);
+    }
+
+    public static BufferedImage loadSprite(String imgPath){
+
+        try {
+            return ImageIO.read(new File(imgPath));
+        } catch (IOException exc){
+            System.out.println("Error opening file: " + exc.getMessage());
+        }
+        return null;
+    }
+
+
+}
