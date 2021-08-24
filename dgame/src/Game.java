fc363f20ede0d1fd180b7d06b3e439f24900209d
Mursal Ashraf <ashrafmursal@gmail.com>
2021-08-24 17:24:08 +1000

    initialised project

---
+++ dgame/src/Game.java
@@ -0,0 +1,22 @@
+public class Game {
+
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 400;
+    public static final int GROUND_HEIGHT = 250;
+
+    private boolean running = false;
+
+    public Game (){
+        // initially empty constructor
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void start(){
+        
+    }
+
+
+}
